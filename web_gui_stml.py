7ab0af4bad6f741fec973ea544a2f609df9dc21b
Alquem <62175166+Alquem@users.noreply.github.com>
2023-09-15 13:58:43 +0100

    Update web_gui_stml.py

--- web_gui_stml.py
+++ web_gui_stml.py
@@ -6,42 +6,6 @@ from PIL import Image
 import numpy as np
 from streamlit_drawable_canvas import st_canvas
 
-# set title
-st.title('Digit classification')
-
-# set header
-st.header('Please draw a single digit number')
-
-# Specify canvas parameters in application
-drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
-)
-
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-if drawing_mode == 'point':
-    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
-
-    
-
-# Create a canvas component
-canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color=bg_color,
-    background_image=Image.open(bg_image) if bg_image else None,
-    update_streamlit=realtime_update,
-    height=150,
-    drawing_mode=drawing_mode,
-    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
-    key="canvas",
-)
-
 def classify(image, model):
     # convert image to (224, 224)
     image = ImageOps.fit(image, (28, 28), Image.Resampling.LANCZOS)
@@ -63,21 +27,62 @@ def classify(image, model):
     
     return np.argmax(res), max(res)
 
-
-# load classifier
-model = load_model('model_mnist.h5')
-
-if canvas_result.image_data is not None:
-
-    st.image(canvas_result.image_data, use_column_width=True)
-
-    # classify image
-    class_name, conf_score = classify(image, model)
-
-    # write classification
-    st.write("## {}".format(class_name))
-    st.write("### score: {}%".format(int(conf_score * 100)))
+def main():
+    # set title
+    st.title('Digit classification')
+    
+    # set header
+    st.header('Please draw a single digit number')
+    
+    # Specify canvas parameters in application
+    drawing_mode = st.sidebar.selectbox(
+        "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
+    )
+    
+    stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+    if drawing_mode == 'point':
+        point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+    stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+    bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+    bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+    
+    realtime_update = st.sidebar.checkbox("Update in realtime", True)
+    
+        
+    
+    # Create a canvas component
+    canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        background_color=bg_color,
+        background_image=Image.open(bg_image) if bg_image else None,
+        update_streamlit=realtime_update,
+        height=150,
+        drawing_mode=drawing_mode,
+        point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+        key="canvas",
+    )
+    
+    
+    
+    
+    # load classifier
+    model = load_model('model_mnist.h5')
+    
+    if canvas_result.image_data is not None:
+    
+        st.image(canvas_result.image_data, use_column_width=True)
+    
+        # classify image
+        class_name, conf_score = classify(image, model)
+    
+        # write classification
+        st.write("## {}".format(class_name))
+        st.write("### score: {}%".format(int(conf_score * 100)))
 
 
 
     
+if __name__ == '__main__':
+    main()
